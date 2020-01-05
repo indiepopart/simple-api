fdc4e76da4ed0a645a36a7314d60a9a1dcfe5da1
indiepopart <jimena@gmail.com>
2020-01-05 19:11:28 -0300

    initial commit

---
+++ src/test/java/com/okta/developer/simpleapi/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.developer.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
